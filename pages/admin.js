417a521ef33efe1393fc7d08a76c7ce747d573c8
yte0 <Yohannes.Tesfay@helse-nord.no>
2022-02-21 17:37:38 +0100

    debug

---
+++ pages/admin.js
@@ -0,0 +1,11 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+const AdminWithNoSSR = dynamic(
+  () =>
+    import("netlify-cms-app").then((CMS) => {
+      CMS.init();
+    }),
+  { ssr: false });
+
+export default AdminWithNoSSR;
