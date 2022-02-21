fdc9b546da81414d95124fe14b4749b7d3567562
yte0 <54272616+yte0@users.noreply.github.com>
2022-02-21 16:26:18 +0000

    Deploying to gh-pages from @ yte0/testpage@c99cd98b9aed4b022de07b8c5b796fb186af2d17 🚀

--- _next/static/2aNuoc02Q1yFmmFvMFEGo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5927965dffee610a51f3.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/admin":["static/chunks/pages/admin-43324193d1f62b7b79eb.js"],sortedPages:["/","/_app","/_error","/admin"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
